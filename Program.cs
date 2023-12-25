2409e3d0aa55e6c664bfff91b79a385629055006
Polina <polinakhlyzova4@gmail.com>
2023-12-25 16:38:40 +0700

    -

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ namespace ConsoleApp1
 
         static void Minus(double a, double b)
         {
-         
+            Console.WriteLine(a - b);
         }
 
         static void Multiply(double a, double b)
