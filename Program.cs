6ba325232ea7c9795af2cdf9ad0de78863bc64ba
Polina <polinakhlyzova4@gmail.com>
2023-12-25 16:29:20 +0700

    menu created

--- Program.cs
+++ Program.cs
@@ -8,8 +8,76 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        private static double A, B;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                PrintMenu();
+                DoAction(Console.ReadLine());
+            }
+        }
+
+        static void PrintMenu() =>
+            Console.WriteLine("1: Input A\n2: Input B\n3: +\n4: -\n5: *\n6: /");
+
+        static void DoAction(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    InputA(Console.ReadLine());
+                    break;
+                case "2":
+                    InputB(Console.ReadLine());
+                    break;
+                case "3":
+                    Plus(A, B);
+                    break;
+                case "4":
+                    Minus(A, B);
+                    break;
+                case "5":
+                    Multiply(A, B);
+                    break;
+                case "6":
+                    Divide(A, B);
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized command.");
+                    break;
+            }
+        }
+
+        static void InputA(string input)
+        {
+
+        }
+
+        static void InputB(string input)
+        {
+
+
+        }
+        static void Plus(double a, double b)
+        {
+
+        }
+
+        static void Minus(double a, double b)
+        {
+
+        }
+
+        static void Multiply(double a, double b)
+        {
+
+        }
+
+        static void Divide(double a, double b)
+        {
+
         }
     }
 }
